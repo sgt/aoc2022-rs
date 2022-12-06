d38fc124b5b2f923681c11b8e5165e7ab2179ca2
Sergei Barbarash <sgt.sgt@gmail.com>
2022-12-06 14:50:10 +0300

    revamp

--- src/common.rs
+++ src/common.rs
@@ -3,22 +3,51 @@ use std::{
     io::{BufRead, BufReader},
 };
 
+use crate::day1;
+use crate::day2;
+use crate::day3;
+use crate::day4;
+use crate::day5;
+use crate::day6;
+
 pub struct Solution {
     day: u8,
+    part: u8,
 }
 
 impl Solution {
-    pub fn new(day: u8) -> Self {
-        Solution { day }
+    pub fn new(day: u8, part: u8) -> Self {
+        Solution { day, part }
     }
 
     fn input_filename(&self) -> String {
         format!("data/input{}.txt", self.day)
     }
 
-    pub(crate) fn read_input(&self) -> Vec<String> {
+    fn read_input(&self) -> Vec<String> {
         let file = File::open(self.input_filename()).expect("no such file");
         BufReader::new(file).lines().map(|l| l.unwrap()).collect()
+    }
+
+    pub fn solve(&self) -> String {
+        let input = self.read_input();
+
+        // let's not get too clever here
+        match self {
+            Self { day: 1, part: 1 } => day1::solution1(&input).to_string(),
+            Self { day: 1, part: 2 } => day1::solution2(&input).to_string(),
+            Self { day: 2, part: 1 } => day2::solution1(&input).to_string(),
+            Self { day: 2, part: 2 } => day2::solution2(&input).to_string(),
+            Self { day: 3, part: 1 } => day3::solution1(&input).to_string(),
+            Self { day: 3, part: 2 } => day3::solution2(&input).to_string(),
+            Self { day: 4, part: 1 } => day4::solution1(&input).to_string(),
+            Self { day: 4, part: 2 } => day4::solution2(&input).to_string(),
+            Self { day: 5, part: 1 } => day5::solution1(&input),
+            Self { day: 5, part: 2 } => day5::solution2(&input),
+            Self { day: 6, part: 1 } => day6::solution1(&input).to_string(),
+            Self { day: 6, part: 2 } => day6::solution2(&input).to_string(),
+            _ => unimplemented!("this solution does not exist yet"),
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -20,29 +20,11 @@ struct Args {
 
 impl Args {
     pub fn solution(&self) -> Solution {
-        Solution::new(self.day)
+        Solution::new(self.day, self.part)
     }
 }
 
 fn main() {
     let args = Args::parse();
-    let input = args.solution().read_input();
-
-    // let's not get too clever here
-    let solution = match args {
-        Args { day: 1, part: 1 } => day1::solution1(&input).to_string(),
-        Args { day: 1, part: 2 } => day1::solution2(&input).to_string(),
-        Args { day: 2, part: 1 } => day2::solution1(&input).to_string(),
-        Args { day: 2, part: 2 } => day2::solution2(&input).to_string(),
-        Args { day: 3, part: 1 } => day3::solution1(&input).to_string(),
-        Args { day: 3, part: 2 } => day3::solution2(&input).to_string(),
-        Args { day: 4, part: 1 } => day4::solution1(&input).to_string(),
-        Args { day: 4, part: 2 } => day4::solution2(&input).to_string(),
-        Args { day: 5, part: 1 } => day5::solution1(&input),
-        Args { day: 5, part: 2 } => day5::solution2(&input),
-        Args { day: 6, part: 1 } => day6::solution1(&input).to_string(),
-        Args { day: 6, part: 2 } => day6::solution2(&input).to_string(),
-        _ => unimplemented!("this solution does not exist yet"),
-    };
-    println!("{}", solution);
+    println!("{}", args.solution().solve());
 }
